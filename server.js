46d866db2e126eaea2b454c4e206145f13d9eec2
ashwaththaman-codes <crystal151069@gmail.com>
2025-06-20 13:55:10 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('signal', (data) => {
+        socket.broadcast.emit('signal', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
